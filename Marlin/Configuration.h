106cbf88201e95e2531e0773c471af94b2e8d875
Juan Chong <juan@highcurrent.io>
2019-11-18 12:45:09 -0500

    Fixed Z probe offset, bed center, and bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -913,7 +913,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -48, 0, -0.90 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
@@ -1044,8 +1044,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 225
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1294,8 +1294,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS -6
+#define MANUAL_Y_HOME_POS -18
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
